39566ccbaf07963e1c9be5fc5f6fd6756e9c3130
Yan Zhao <yan@yan-G60VX.(none)>
2013-02-17 19:14:28 -0800

    Reads the csv file and creates new people for each person in the csv file. The default ranking is 0. Need to check if the format will always be the same, as well as what ranking to add.

--- src/Parser.java
+++ src/Parser.java
@@ -1,24 +1,41 @@
 import java.util.ArrayList;
-
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import java.util.List;
+import java.util.Scanner;
 
 public class Parser {
 	
-	public List fileToParse;
+	public List<String[]> fileToParse;
 	
-	public Parser(List fileToParse) {
+	public Parser(List<String[]> fileToParse) {
 		this.fileToParse = fileToParse;
 	}
 	
 	public ArrayList<Person> parseForPeople() {
 		ArrayList<Person> arrayOfPeople = new ArrayList<Person>();
 		
-		//Making the assumption that the 2nd column in the cvs file is the
+		//Making the assumption that the 2nd column in the csv file is the
 		//name, with the 3rd column being the email address.
-		
-		//TODO: Index 0 of each String[] in fileToParse represents a row.
-		//Split based on commas (each comma is a column). Get the necessary
-		//information, create a person, add that person to the list.
+		//Trim removes the leading and trailing whitespace, if any. 
+		for (int i = 1; i < fileToParse.size(); i++) {
+			String[] containsRow = fileToParse.get(i);
+			
+			Scanner scanRow = new Scanner(containsRow[0]);  
+			scanRow.useDelimiter(",");
+			
+			//Skips over the timestamp.
+			scanRow.next();
+			String name = scanRow.next();
+			name = name.trim();
+			
+			String email = scanRow.next();
+			email = email.trim();
+			
+			//temporarily setting ranking to 0.
+			int ranking = 0;
+			
+			Person personToAdd = new Person(name, email, ranking);
+			arrayOfPeople.add(personToAdd);
+		}
 		
 		//Things to look into: Determining if the comma is referring to the 
 		//column split or the comma in the column.
--- src/Teambuilder.java
+++ src/Teambuilder.java
@@ -1,8 +1,8 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import com.googlecode.jcsv.reader.CSVReader;
 import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
@@ -10,7 +10,6 @@ import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
 public class Teambuilder {
     public static void main(String[] args) {
         try {
-        	System.out.println(args[0]);
         	FileReader file = new FileReader(args[0]);
 			CSVReader<String[]> csvParser = CSVReaderBuilder.newDefaultReader(file);
 			
@@ -20,14 +19,9 @@ public class Teambuilder {
 			List<String[]> fileContents = csvParser.readAll();
 			
 			//Need to call parser method to parse information.
+			Parser toParse = new Parser(fileContents);
 			
-			//Debugging purposes here to see how the csv parser works.
-			int i = 1;
-			for (String[] strings : fileContents) {
-				System.out.println("Row number: " + i);
-				//Each row is stored at index 0.
-				System.out.println(strings[0]);
-			}
+			toParse.parseForPeople();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
